d7c7bc97a5340f8bf5861fb538da02236d3e40f7
Palash <palashsharma359@gmail.com>
2024-08-14 21:44:24 +0530

    weather app

--- src/components/WeatherComponent.jsx
+++ src/components/WeatherComponent.jsx
@@ -31,20 +31,20 @@ const WeatherComponent = () => {
          <button style={{ height: '35px', marginTop: '10px' }} onClick={handleSearch}>Search</button>
           <p>{loaded}</p>          
          {Object.keys(cityWeather).length!==0 &&
-            <div style={{ display: 'flex', justifyContent: 'space-around', marginTop: '40px' }}>
-               <div style={{ width: '150px', height: '150px', border: '1px solid black' }}>
+            <div className="weather-cards" style={{ display: 'flex', justifyContent: 'space-around', marginTop: '40px' }}>
+               <div className="weather-card" style={{ width: '150px', height: '150px', border: '1px solid black' }}>
                   <h3>Temperature</h3>
                   <p>{cityWeather.current.temp_c}&#8451;</p>
                </div>
-               <div style={{ width: '150px', height: '150px', border: '1px solid black' }}>
+               <div className="weather-card" style={{ width: '150px', height: '150px', border: '1px solid black' }}>
                   <h3>Humidity</h3>
                   <p>{cityWeather.current.humidity}%</p>
                </div>
-               <div style={{ width: '150px', height: '150px', border: '1px solid black' }}>
+               <div className="weather-card" style={{ width: '150px', height: '150px', border: '1px solid black' }}>
                   <h3>Condition</h3>
                   <p>{cityWeather.current.condition.text}</p>
                </div>
-               <div style={{ width: '150px', height: '150px', border: '1px solid black' }}>
+               <div className="weather-card" style={{ width: '150px', height: '150px', border: '1px solid black' }}>
                   <h3>Wind Speed</h3>
                   <p>{cityWeather.current.wind_kph} kph</p>
                </div>
